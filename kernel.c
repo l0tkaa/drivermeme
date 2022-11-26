9e5d35f0d93d8812181e2a10da86afde5c793a59
l0tkaa <106283020+l0tkaa@users.noreply.github.com>
2022-11-26 14:46:01 -0600

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,8 +1,9 @@
-#https://tldp.org/LDP/lkmpg/2.6/html/lkmpg.html#AEN121
-#https://stackoverflow.com/questions/22632713/how-to-write-a-simple-linux-device-driver
-#https://www.apriorit.com/dev-blog/645-lin-linux-wi-fi-driver-tutorial-how-to-write-simple-linux-wireless-driver-prototype
-#https://www.linuxjournal.com/article/2476
-#https://www.oreilly.com/library/view/linux-device-drivers/0596005903/ch17.html
+/* https://tldp.org/LDP/lkmpg/2.6/html/lkmpg.html#AEN121
+https://stackoverflow.com/questions/22632713/how-to-write-a-simple-linux-device-driver
+https://www.apriorit.com/dev-blog/645-lin-linux-wi-fi-driver-tutorial-how-to-write-simple-linux-wireless-driver-prototype
+https://www.linuxjournal.com/article/2476
+https://www.oreilly.com/library/view/linux-device-drivers/0596005903/ch17.html */
+
 
 /*  
  *  hello-1.c - The simplest kernel module.
